0c0b7cc7e20c9e92ac53a108bf8f40e5e3f78c64
Vladislav Botvin <darkvlados@gmail.com>
2020-06-13 12:38:56 +0300

    improve tests

--- index.test.ts
+++ index.test.ts
@@ -2,35 +2,50 @@ import { rocketPipe } from ".";
 import { Either } from "monet";
 
 describe("Rocket pipes tests", () => {
-  it("Simple test", async () => {
-    const resp = await rocketPipe(
-      () => 123,
-      (n) => n + 1
-    )();
-    expect(resp).toEqual(124);
+  describe("Simple", () => {
+    it("Simple test", async () => {
+      const resp = await rocketPipe(
+        () => 123,
+        (n) => n + 1
+      )();
+      expect(resp).toEqual(124);
+    });
   });
 
-  it("Promise pass test", async () => {
-    const resp = await rocketPipe(
-      () => Promise.resolve(123),
-      (n) => n + 1
-    )();
-    expect(resp).toEqual(124);
+  describe("Promise", () => {
+    it("Promise pass test", async () => {
+      const resp = await rocketPipe(
+        () => Promise.resolve(123),
+        (n) => n + 1
+      )();
+      expect(resp).toEqual(124);
+    });
+
+    it("Promise reject test", async () => {
+      expect(
+        rocketPipe(
+          () => Promise.reject(123),
+          (n) => "qwe"
+        )()
+      ).rejects.toEqual(123);
+    });
   });
 
-  it("Either right pass test", async () => {
-    const resp = await rocketPipe(
-      () => Either.right(123),
-      (n) => n + 1
-    )();
-    expect(resp).toEqual(124);
-  });
+  describe("Monet", () => {
+    it("Either right pass test", async () => {
+      const resp = await rocketPipe(
+        () => Either.right(123),
+        (n) => n + 1
+      )();
+      expect(resp).toEqual(124);
+    });
 
-  it("Either right in promise pass test", async () => {
-    const resp = await rocketPipe(
-      () => Promise.resolve(Either.right(123)),
-      (n) => n + 1
-    )();
-    expect(resp).toEqual(124);
+    it("Either right in promise pass test", async () => {
+      const resp = await rocketPipe(
+        () => Promise.resolve(Either.right(123)),
+        (n) => n + 1
+      )();
+      expect(resp).toEqual(124);
+    });
   });
 });
